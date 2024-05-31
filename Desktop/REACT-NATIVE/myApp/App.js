2b0cfd9efd4e3934b90e384a1ea668d20a79c544
dr-akoto <dr-akotoosei@gmail.com>
2024-05-31 13:42:45 +0000

    created the profile picture

--- Desktop/REACT-NATIVE/myApp/App.js
+++ Desktop/REACT-NATIVE/myApp/App.js
@@ -1,10 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View, Image } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.header}>
+//         <View>
+//           <Text style={styles.text}>Hello , Devs</Text>
+//           <Text>14 tasks today</Text>
+//         </View>
+//         <View style={styles.image}>
+//           <Image
+//             style={styles.tinyLogo}
+//             source={require("./assets/images/profile.png")}
+//           />
+//         </View>
+//       </View>
+
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#f7f0e8",
+//     // alignItems: "center",
+//     // justifyContent: "center",
+//   },
+//   header: {
+//     display: "flex",
+//     flexDirection: "column",
+//   },
+//   text: {
+//     fontSize: 30,
+//     fontWeight: "bold",
+//     // backgroundColor:"#000"
+//   },
+//   tinyLogo: {
+//     width: 80,
+//     height: 80,
+//     borderRadius: 100, // Makes the image circular
+
+//     borderColor: "#000",
+//     backgroundColor: "white",
+//   },
+// });
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.text}>Hello, Devs</Text>
+          <Text>14 tasks today</Text>
+        </View>
+        <View style={styles.image}>
+          <Image
+            style={styles.tinyLogo}
+            source={require("./assets/images/profile.png")}
+          />
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +74,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7f0e8',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f7f0e8",
+    padding: 12,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    // backgroundColor:"#000"
+  },
+  tinyLogo: {
+    width: 80,
+    height: 80,
+    borderRadius: 100, // Makes the image circular
+    borderColor: "#000",
+    backgroundColor: "white",
   },
 });
